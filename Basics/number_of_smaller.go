8aed7d93722938038d809eda4c3bc565887e1962
NJrslv <108277031+NJrslv@users.noreply.github.com>
2023-06-12 10:23:27 +0300

    Update number_of_smaller.go

--- Basics/number_of_smaller.go
+++ Basics/number_of_smaller.go
@@ -1,3 +1,16 @@
+/*
+You are given two arrays, sorted in non-decreasing order. For each element of the second array, 
+find the number of elements in the first array strictly less than it.
+
+Input
+The first line contains integers 𝑛 and 𝑚, the sizes of the arrays (1≤𝑛,𝑚≤105). 
+The second line contains 𝑛 integers 𝑎𝑖, elements of the first array, 
+the third line contains 𝑚 integers 𝑏𝑖, elements of the second array (−109≤𝑎𝑖,𝑏𝑖≤109).
+
+Output
+Print 𝑚 numbers, the number of elements of the first array less than each of the elements of the second array.
+*/
+
 package main
  
 import (
